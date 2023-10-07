20b3328c7e8991e0caa7f4c58736b652fff8c121
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-07 02:37:09 -0700

    feat: appointment 단위 테스트 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { AppointmentService } from './appointment/appointment.service';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, PatientModule],
   controllers: [AppController],
   providers: [AppService, AppointmentService],
 })
--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -33,4 +33,87 @@ describe('AppointmentService', () => {
 			confirmed: false,
 		});
 	});
+
+	// 에러가 나는 테스트코드 즉, toThrowError는 해당 startTime과 endTime으로 인해서 발생하는
+	// 에러가 발생해야하고, 그리고 appointment's endTime should be after startTime 이러한 에러 메세지가 나와야함.
+	it('should throw an error when end time is before start time', () => {
+		const startTime = new Date('2022-01-01T14:00:00Z');
+		const endTime = new Date('2022-01-01T13:00:00Z');
+		/**
+		 * We have to wrap our "scheduleAppointment" function in another arrow function
+		 * because we expect an error to be thrown. If we don't do that,
+		 * Jest won't be able to properly handle the error and it will accuse that the test failed.
+		 */
+		expect(() =>
+			service.scheduleAppointment({
+				patientId: 1,
+				startTime,
+				endTime,
+			}),
+		).toThrowError("appointment's endTime should be after startTime");
+	});
+
+	// 시작시간과 끝나는시간이 같을때 에러
+	it('should throw an error when end time is equal to start time', () => {
+		const startTime = new Date('2022-01-01T14:00:00Z');
+		const endTime = startTime;
+
+		expect(() =>
+			service.scheduleAppointment({
+				patientId: 1,
+				startTime,
+				endTime,
+			}),
+		).toThrowError("appointment's endTime should be after startTime");
+	});
+
+	// 시작시간 일자와 끝나는시간 일자가 다를때 에러
+	it('should throw an error when end time is in the next day', () => {
+		const startTime = new Date('2022-01-01T14:00:00Z');
+		const endTime = new Date('2022-01-02T14:00:00Z');
+
+		expect(() =>
+			service.scheduleAppointment({
+				patientId: 1,
+				startTime,
+				endTime,
+			}),
+		).toThrowError(
+			"appointment's endTime should be in the same day as start time's",
+		);
+	});
+
+	// 시작시간 월수와 끝나는시간 월수가 다를때 에러
+	it('should throw an error when end time is in same day and hour of next month', () => {
+		const startTime = new Date('2022-01-01T14:00:00Z');
+		const endTime = new Date('2022-02-01T14:00:00Z');
+
+		expect(() =>
+			service.scheduleAppointment({
+				patientId: 1,
+				startTime,
+				endTime,
+			}),
+		).toThrowError(
+			"appointment's endTime should be in the same day as start time's",
+		);
+	});
+
+	// 시작시간 년도와 끝나는시간 년도가 다를때 에러
+	it('should throw an error when end time is in same day, hour and month of the next year', () => {
+		const startTime = new Date('2022-01-01T14:00:00Z');
+		const endTime = new Date('2023-01-01T14:00:00Z');
+
+		expect(() =>
+			service.scheduleAppointment({
+				patientId: 1,
+				startTime,
+				endTime,
+			}),
+		).toThrowError(
+			"appointment's endTime should be in the same day as start time's",
+		);
+	});
+
+	// 여기까지가 단위테스트
 });
