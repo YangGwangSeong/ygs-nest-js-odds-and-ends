c55f0e8ac6ee67a4ac941f5e0b963a6e4feddc81
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-07 01:37:12 -0700

    feat: An unconfirmed schedule should be created on success

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { AppointmentService } from './appointment/appointment.service';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppointmentService],
 })
 export class AppModule {}
