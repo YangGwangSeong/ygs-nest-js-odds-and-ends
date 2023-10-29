6d4f776c7d0d4724782f83b5198c09556af1d799
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-29 04:44:21 -0700

    feat: nestjs-tdd-tutorial

--- src/appointment/appointment.service.spec.ts
+++ src/appointment/appointment.service.spec.ts
@@ -24,13 +24,11 @@ describe('AppointmentService', () => {
 	it('should schedule an unconfirmed appointment for a user on success', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
 		const endTime = new Date('2022-01-01T15:00:00Z');
-
-		// Using the `register` method to retrieve the new patient id
 		const { id: patientId } = await patientService.register({
 			name: 'John Doe',
 		});
 
-		const newAppointment = service.scheduleAppointment({
+		const newAppointment = await service.scheduleAppointment({
 			patientId,
 			startTime,
 			endTime,
@@ -44,83 +42,73 @@ describe('AppointmentService', () => {
 		});
 	});
 
-	// 에러가 나는 테스트코드 즉, toThrowError는 해당 startTime과 endTime으로 인해서 발생하는
-	// 에러가 발생해야하고, 그리고 appointment's endTime should be after startTime 이러한 에러 메세지가 나와야함.
-	it('should throw an error when end time is before start time', () => {
+	it('should throw an error when end time is before start time', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
 		const endTime = new Date('2022-01-01T13:00:00Z');
-		/**
-		 * We have to wrap our "scheduleAppointment" function in another arrow function
-		 * because we expect an error to be thrown. If we don't do that,
-		 * Jest won't be able to properly handle the error and it will accuse that the test failed.
-		 */
-		expect(() =>
+
+		await expect(
 			service.scheduleAppointment({
 				patientId: 1,
 				startTime,
 				endTime,
 			}),
-		).toThrowError("appointment's endTime should be after startTime");
+		).rejects.toThrowError("appointment's endTime should be after startTime");
 	});
 
-	// 시작시간과 끝나는시간이 같을때 에러
-	it('should throw an error when end time is equal to start time', () => {
+	it('should throw an error when end time is equal to start time', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
 		const endTime = startTime;
 
-		expect(() =>
+		await expect(
 			service.scheduleAppointment({
 				patientId: 1,
 				startTime,
 				endTime,
 			}),
-		).toThrowError("appointment's endTime should be after startTime");
+		).rejects.toThrowError("appointment's endTime should be after startTime");
 	});
 
-	// 시작시간 일자와 끝나는시간 일자가 다를때 에러
-	it('should throw an error when end time is in the next day', () => {
+	it('should throw an error when end time is in the next day', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
-		const endTime = new Date('2022-01-02T14:00:00Z');
+		const endTime = new Date('2022-01-02T00:00:00Z');
 
-		expect(() =>
+		await expect(
 			service.scheduleAppointment({
 				patientId: 1,
 				startTime,
 				endTime,
 			}),
-		).toThrowError(
+		).rejects.toThrowError(
 			"appointment's endTime should be in the same day as start time's",
 		);
 	});
 
-	// 시작시간 월수와 끝나는시간 월수가 다를때 에러
-	it('should throw an error when end time is in same day and hour of next month', () => {
+	it('should throw an error when end time is in same day and hour of next month', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
 		const endTime = new Date('2022-02-01T14:00:00Z');
 
-		expect(() =>
+		await expect(
 			service.scheduleAppointment({
 				patientId: 1,
 				startTime,
 				endTime,
 			}),
-		).toThrowError(
+		).rejects.toThrowError(
 			"appointment's endTime should be in the same day as start time's",
 		);
 	});
 
-	// 시작시간 년도와 끝나는시간 년도가 다를때 에러
-	it('should throw an error when end time is in same day, hour and month of the next year', () => {
+	it('should throw an error when end time is in same day, hour and month of the next year', async () => {
 		const startTime = new Date('2022-01-01T14:00:00Z');
 		const endTime = new Date('2023-01-01T14:00:00Z');
 
-		expect(() =>
+		await expect(
 			service.scheduleAppointment({
 				patientId: 1,
 				startTime,
 				endTime,
 			}),
-		).toThrowError(
+		).rejects.toThrowError(
 			"appointment's endTime should be in the same day as start time's",
 		);
 	});
