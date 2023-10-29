a2287b3d4db253a17014e10e5bae75cc40a452e4
YangGwangSeong <rhkdtjd_12@naver.com>
2023-10-29 05:22:44 -0700

    feat: 테스트 파일들을 test 폴더에 모으기 위한 경로 설정

--- nest-tdd-tutorial/src/app.module.ts
+++ nest-tdd-tutorial/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesService } from './api/articles/articles.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ArticlesService],
 })
 export class AppModule {}
