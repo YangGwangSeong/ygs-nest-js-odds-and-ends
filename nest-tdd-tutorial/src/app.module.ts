a040bee1e984ce022b195cadcd1aabd183bf0e90
YangGwangSeong <rhkdtjd_12@naver.com>
2023-11-10 23:57:50 -0800

    feat: test폴더로 이동 후 test e2e 통과

--- nest-tdd-tutorial/src/app.module.ts
+++ nest-tdd-tutorial/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArticlesService } from './api/articles/articles.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, ArticlesService],
+	imports: [],
+	controllers: [],
+	providers: [ArticlesService],
 })
 export class AppModule {}
