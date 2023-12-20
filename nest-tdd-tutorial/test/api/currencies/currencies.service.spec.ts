6cd8d07e0df39747762728ac4f4d9237b6e1fb0e
YangGwangSeong <rhkdtjd_12@naver.com>
2023-12-20 03:25:48 -0800

    feat: getCurrency throw error and not throw if repository

--- nest-tdd-tutorial/test/api/currencies/currencies.service.spec.ts
+++ nest-tdd-tutorial/test/api/currencies/currencies.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from '@/api/currencies/currencies.service';
+import {
+	CurrenciesRepository,
+	CurrenciesService,
+} from '@/api/currencies/currencies.service';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('CurrenciesService', () => {
 	let service: CurrenciesService;
+	let repository: CurrenciesRepository;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [CurrenciesService],
+			providers: [
+				CurrenciesService,
+				{
+					provide: CurrenciesRepository,
+					useFactory: () => ({ getCurrency: jest.fn() }),
+				},
+			],
 		}).compile();
 
 		service = module.get<CurrenciesService>(CurrenciesService);
+		repository = module.get<CurrenciesRepository>(CurrenciesRepository);
 	});
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	describe('getCurrency()', () => {
+		it('should be throw if repository throw', async () => {
+			//repository의 getCurrency 메소드를 비동이 실패값을 mockRejectedValue로 반환해줌
+			(repository.getCurrency as jest.Mock).mockRejectedValue(
+				new InternalServerErrorException(),
+			);
+			await expect(service.getCurrency('INVALID')).rejects.toThrow(
+				new InternalServerErrorException(),
+			);
+		});
+
+		//is repository
+		it('should be not throw if repository returns', async () => {
+			await expect(service.getCurrency('USD')).resolves.not.toThrow();
+		});
 	});
 });
