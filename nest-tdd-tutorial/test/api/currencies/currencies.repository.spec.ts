0404b14887a982d94026db3cd7d700459fd1d6ac
YangGwangSeong <rhkdtjd_12@naver.com>
2024-01-11 02:00:23 -0800

    test(nest-tdd-rutorial): createCurrency 메소드 3-1 repository create 메소드 파라미터 맞는지

--- nest-tdd-tutorial/test/api/currencies/currencies.repository.spec.ts
+++ nest-tdd-tutorial/test/api/currencies/currencies.repository.spec.ts
@@ -14,6 +14,7 @@ import { Repository } from 'typeorm';
 describe('CurrenciesRepository', () => {
 	let reposiotry: CurrenciesRepository;
 	let currenciesRepository: Repository<Currencies>;
+	let mockData: Currencies;
 
 	beforeEach(async () => {
 		const CurrenciesRepositoryMock = {
@@ -34,6 +35,7 @@ describe('CurrenciesRepository', () => {
 		currenciesRepository = module.get<Repository<Currencies>>(
 			getRepositoryToken(Currencies), // 여기에도 꼭 getRepositoryToken 함수 사용
 		);
+		mockData = { currency: 'USD', value: 1 };
 	});
 
 	// 1. 해당 service든 repository든 정의 되어 있는지 체크
@@ -62,9 +64,24 @@ describe('CurrenciesRepository', () => {
 
 		// 2-3. findOneBy 정상적으로 동작하는 return값 Promise 리턴 받으니까 await 붙여야함
 		it('should be returns when findOne returns', async () => {
-			const mockData = { currency: 'USD', value: 1 } as Currencies;
 			(currenciesRepository.findOneBy as jest.Mock).mockReturnValue(mockData);
 			expect(await reposiotry.getCurrency('USD')).toEqual(mockData);
+		});
+	});
+
+	// 3. createCurrency 메소드
+	describe('createCurrency()', () => {
+		// beforEach는 해당 테스트가 실행되기전에 콜백 함수안에 있는것들을 실행 해주는거
+		// save는 해당 테스트에서만 사용 되니까 해당 테스트 코드 안에서만 미리 불러올 수 있게 beforEach로 모킹 함수를 생성한것 같다!
+		beforeEach(() => {
+			currenciesRepository.save = jest.fn();
+		});
+
+		// 3-1 repository create 메소드 파라미터 맞는지
+		it('should be called save with correct params', async () => {
+			currenciesRepository.save = jest.fn().mockReturnValue(mockData);
+			await reposiotry.createCurrency(mockData);
+			expect(currenciesRepository.save).toBeCalledWith(mockData);
 		});
 	});
 });
