02bcdbb9b035e05ea8d47a4aca4716a69db0f8fc
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-03 22:10:32 -0800

    feat(nestjs-e2e-tutorial): payments module controller 생성

--- nestjs-e2e-tutorial/src/app.module.ts
+++ nestjs-e2e-tutorial/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [CustomersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
