207dc050072645421ae62a9b26abfebaea0998b1
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-03 22:36:22 -0800

    feat(nestjs-e2e-tutorial): should return a status of 400

--- nestjs-e2e-tutorial/src/payments/controllers/payments/payments.controller.spec.ts
+++ nestjs-e2e-tutorial/src/payments/controllers/payments/payments.controller.spec.ts
@@ -1,8 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
+import { Request, Response } from 'express';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+
+  const requestMock = {
+    query: {},
+  } as unknown as Request;
+
+  const statusResponseMock = {
+    send: jest.fn((x) => x),
+  };
+
+  const responseMock = {
+    status: jest.fn(() => statusResponseMock),
+    send: jest.fn((x) => x),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +28,17 @@ describe('PaymentsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // 1. getPaymensts
+  describe('getPayments', () => {
+    // 1-1 should return a status of 400
+    it('should return a status of 400', () => {
+      controller.getPayments(requestMock, responseMock); // 파라미터 request와 response를 전달 해주어야 하는데 그 부분을 Mock으로 만든다.
+      expect(responseMock.status).toHaveBeenCalledWith(400);
+      expect(statusResponseMock.send).toHaveBeenCalledWith({
+        msg: 'Missing count or page query parameter',
+      });
+    });
   });
 });
--- nestjs-e2e-tutorial/src/payments/controllers/payments/payments.controller.ts
+++ nestjs-e2e-tutorial/src/payments/controllers/payments/payments.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+  @Get()
+  getPayments(@Req() request: Request, @Res() response: Response) {
+    const { count, page } = request.query;
+
+    if (!count || !page) {
+      response
+        .status(400)
+        .send({ msg: 'Missing count or page query parameter' });
+    } else {
+      response.send(200);
+    }
+  }
+}
