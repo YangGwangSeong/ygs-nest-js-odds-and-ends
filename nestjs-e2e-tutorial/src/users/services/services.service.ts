0b65ea2085270bc11681cd9334728a286d3054f6
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-04 23:44:42 -0800

    feat(nestjs-e2e-tutorial): getUsers method 추가

--- nestjs-e2e-tutorial/src/users/services/services.service.ts
+++ nestjs-e2e-tutorial/src/users/services/services.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../types/User';
 
 @Injectable()
-export class ServicesService {}
+export class ServicesService {
+  private users: User[] = [
+    {
+      username: 'anson',
+      password: 'anson',
+    },
+    {
+      username: 'danny',
+      password: 'ansodannyn',
+    },
+    {
+      username: 'dreak',
+      password: 'dreak',
+    },
+  ];
+
+  getUsers() {
+    return this.users;
+  }
+}
