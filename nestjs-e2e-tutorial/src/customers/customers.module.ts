d02514e1d8edea79b81a057cf5c7355fadb3840b
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-05 21:42:36 -0800

    feat(nestjs-e2e-tutorial): Middleware - validate-customer-account.moddleware 추가

--- nestjs-e2e-tutorial/src/customers/customers.module.ts
+++ nestjs-e2e-tutorial/src/customers/customers.module.ts
@@ -7,6 +7,7 @@ import {
 import { CustomersController } from './controllers/customers/customers.controller';
 import { CustomersService } from './services/customers/customers.service';
 import { ValidateCutomerModdleware } from './middlewares/validate-customer.middleware';
+import { ValidateCustomerAccountMiddleware } from './middlewares/validate-customer-account.middleware';
 
 @Module({
   controllers: [CustomersController],
@@ -20,7 +21,7 @@ import { ValidateCutomerModdleware } from './middlewares/validate-customer.middl
 export class CustomersModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(ValidateCutomerModdleware)
+      .apply(ValidateCutomerModdleware, ValidateCustomerAccountMiddleware)
       .exclude({
         path: 'create',
         method: RequestMethod.POST,
