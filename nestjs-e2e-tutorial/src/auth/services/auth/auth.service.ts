d0c1db4540cc719fc70b33dab82859d823e0b688
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-05 23:57:06 -0800

    feat(nestjs-e2e-tutorial): Authentication with PassportJS - auth-service validateUser 메서드 추가

--- nestjs-e2e-tutorial/src/auth/services/auth/auth.service.ts
+++ nestjs-e2e-tutorial/src/auth/services/auth/auth.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('USER_SERVICE')
+    private readonly usersService: UsersService,
+  ) {}
+  validateUser(username: string, password: string) {}
+}
