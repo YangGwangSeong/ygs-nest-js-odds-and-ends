998fdec212a2553a6c5b3639fc88e2613aa61166
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-07 04:04:46 -0800

    feat(nestjs-e2e-tutorial): userService Unit test - 2. should call userRepository.create with correct params

--- nestjs-e2e-tutorial/src/users/services/users.service.spec.ts
+++ nestjs-e2e-tutorial/src/users/services/users.service.spec.ts
@@ -40,11 +40,12 @@ describe('UsersService', () => {
 
   // userService Unit test
   describe('createUser', () => {
+    jest.spyOn(bcyptUils, 'encodePassword').mockReturnValue('hashed123');
+
     // userService Unit test - 1. should encode password correctly
     // 내 생각에 여기에서 실제 결과값을 얻을 수 있는 spyOn을 이용해서 해시값이 잘 생성되는지 테스트 한것 같다.
     // 어떤 부분을 mocking 할것이고 어떤것을 spyOn 할것인지가 중요한것 같다.
     it('should encode password correctly', async () => {
-      jest.spyOn(bcyptUils, 'encodePassword').mockReturnValueOnce('hashed123');
       await service.createUser({
         username: 'anson',
         email: 'anson@gmail.com',
@@ -52,6 +53,21 @@ describe('UsersService', () => {
       });
 
       expect(bcyptUils.encodePassword).toHaveBeenCalledWith('123');
+    });
+
+    // userService Unit test - 2. should call userRepository.create with correct params
+    it('should call userRepository.create with correct params', async () => {
+      await service.createUser({
+        username: 'anson',
+        email: 'anson@gmail.com',
+        password: '123',
+      });
+
+      expect(userRepository.create).toHaveBeenCalledWith({
+        username: 'anson',
+        email: 'anson@gmail.com',
+        password: 'hashed123',
+      });
     });
   });
 });
