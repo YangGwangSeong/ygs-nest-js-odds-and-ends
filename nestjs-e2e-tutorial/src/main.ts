29e6b0adc664054c8b723cc222de8be5e47e40e3
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-06 22:55:00 -0800

    feat(nestjs-e2e-tutorial): Sessions

--- nestjs-e2e-tutorial/src/main.ts
+++ nestjs-e2e-tutorial/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      name: 'NESTJS_SESSION_ID',
+      secret: '#$1234124',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
