0b0ed9f836830cf03cdc4dd81698c23ef237f70b
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-07 23:38:08 -0800

    feat(nestjs-e2e-tutorial): 2. Authentication

---
+++ nestjs-e2e-tutorial/test/auth.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('UsersController E2E Test', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  // 2. Authentication
+  describe('Authentication', () => {});
+});
