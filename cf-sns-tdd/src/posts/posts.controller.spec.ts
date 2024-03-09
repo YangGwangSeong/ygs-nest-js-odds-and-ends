6ef46ebf552db0287bfde321d683f30ffa7abf54
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-09 04:26:24 -0800

    feat(cf-sns-tdd): (Posts 모듈 생성하기)1-2 getPost 메소드의 리턴값이 맞는지 체크

--- cf-sns-tdd/src/posts/posts.controller.spec.ts
+++ cf-sns-tdd/src/posts/posts.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PostsController } from './posts.controller';
+import { IPost, PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
@@ -23,6 +23,19 @@ describe('PostsController', () => {
     // 1-1 getPost 메소드가 정의 되었는지
     it('should be defined getPost', () => {
       expect(controller.getPost).toBeDefined();
+    });
+    const returnMockValue: IPost = {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 10000000,
+      commentCount: 999999,
+    };
+    // 1-2 getPost 메소드의 리턴값이 맞는지 체크
+    it('should return a post', () => {
+      const result = controller.getPost();
+
+      expect(result).toEqual(returnMockValue);
     });
   });
 });
--- cf-sns-tdd/src/posts/posts.controller.ts
+++ cf-sns-tdd/src/posts/posts.controller.ts
@@ -14,5 +14,13 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost() {}
+  getPost() {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 10000000,
+      commentCount: 999999,
+    };
+  }
 }
