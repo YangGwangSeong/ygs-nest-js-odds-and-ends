7d33ce98492292795a6141b79fcae6c9fe59678a
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-17 01:57:50 -0700

    feat(cf-sns-tdd): (NestJS에 Typeorm 설정하기) NestJS에 Typeorm 설정하기

--- cf-sns-tdd/src/app.module.ts
+++ cf-sns-tdd/src/app.module.ts
@@ -1,18 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5437,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/../.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const option = {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get<number>('DB_PORT')),
+          username: configService.get('DB_USERNAME'),
+          database: configService.get('DB_DATABASE'),
+          password: configService.get('DB_PASSWORD'),
+          entities: [],
+          synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+        };
+
+        return option;
+      },
     }),
     PostsModule,
   ],
