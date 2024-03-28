25645099051122cc5a5b115141448a80414fb1f5
YangGwangSeong <rhkdtjd_12@naver.com>
2024-03-27 23:36:10 -0700

    feat(cf-sns-tdd): (User 관련 서비스 및 컨트롤러 기능 작성하기) users-repository createUserRepository 메서드 unit test

--- cf-sns-tdd/src/users/users.repository.spec.ts
+++ cf-sns-tdd/src/users/users.repository.spec.ts
@@ -3,14 +3,20 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
 import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { RolesEnum } from './const/roles.const';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('UsersRepository', () => {
   let repository: UsersRepository;
   let usersRepository: Repository<UsersModel>;
+  let mockData: UsersModel;
+  let createUserDtoArgs: CreateUserDto;
 
   beforeEach(async () => {
     const UsersRepositoryMock = {
       find: jest.fn(),
+      save: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -27,6 +33,20 @@ describe('UsersRepository', () => {
     usersRepository = module.get<Repository<UsersModel>>(
       getRepositoryToken(UsersModel),
     );
+
+    mockData = {
+      id: 1,
+      nickname: 'factory',
+      email: 'rhkdtjd_12@naver.com',
+      password: 'factory',
+      role: RolesEnum.USER,
+    };
+
+    createUserDtoArgs = {
+      nickname: mockData.nickname,
+      email: mockData.email,
+      password: mockData.password,
+    };
   });
 
   it('should be defined repository', () => {
@@ -49,6 +69,41 @@ describe('UsersRepository', () => {
       usersRepository.find = jest.fn().mockReturnValue([]);
 
       expect(await repository.getUsersRepository()).toEqual([]);
+    });
+  });
+
+  // 2. createUserRepository()
+  describe('createUserRepository()', () => {
+    // 2-1 createUserRepository 메서드가 정의 되었는지
+    it('createUserRepository 메서드가 정의 되었는지', () => {
+      expect(repository.createUserRepository).toBeDefined();
+    });
+
+    // 2-2 파라미터값이 올바른지 확인
+    it('파라미터값이 올바른지 확인', async () => {
+      usersRepository.save = jest.fn().mockReturnValue(mockData);
+      await repository.createUserRepository(createUserDtoArgs);
+      expect(usersRepository.save).toHaveBeenCalledWith(createUserDtoArgs);
+    });
+
+    // 2-3 repository save 함수가 에러가 났을때
+    it('repository save 함수가 에러가 났을때', async () => {
+      usersRepository.save = jest
+        .fn()
+        .mockRejectedValue(new InternalServerErrorException());
+
+      await expect(
+        repository.createUserRepository(createUserDtoArgs),
+      ).rejects.toThrow(new InternalServerErrorException());
+    });
+
+    // 2-4 save함수 성공시 리턴값
+    it('save함수 성공시 리턴값', async () => {
+      usersRepository.save = jest.fn().mockResolvedValue(mockData);
+
+      expect(await repository.createUserRepository(createUserDtoArgs)).toEqual(
+        mockData,
+      );
     });
   });
 });
--- cf-sns-tdd/src/users/users.repository.ts
+++ cf-sns-tdd/src/users/users.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UsersModel } from './entities/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<UsersModel> {
@@ -14,5 +15,13 @@ export class UsersRepository extends Repository<UsersModel> {
 
   async getUsersRepository() {
     return await this.repository.find();
+  }
+
+  async createUserRepository(createUserArgs: CreateUserDto) {
+    return await this.repository.save({
+      nickname: createUserArgs.nickname,
+      email: createUserArgs.email,
+      password: createUserArgs.password,
+    });
   }
 }
